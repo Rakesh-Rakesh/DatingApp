37ddcbb493dddcdcdfb7c3c75dce461ea10f88db
Rakesh <rakeshjak225@gmail.com>
2024-03-11 22:25:36 +0530

    End of section 6

---
+++ client/src/app/members/member-deatil/member-deatil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-deatil',
+  templateUrl: './member-deatil.component.html',
+  styleUrls: ['./member-deatil.component.css']
+})
+export class MemberDeatilComponent {
+
+}
