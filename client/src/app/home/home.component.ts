ce26f3f9b3b334f9f341a81907cea042c90c2379
Rakesh <rakeshjak225@gmail.com>
2024-06-18 17:08:03 +0530

    End of section 3

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -27,7 +27,7 @@ getUsers(){
   this.http.get('https://localhost:5001/api/users').subscribe({
     next: (response) => (this.users = response),
     error: (error) => console.log(error),
-    complete: () => console.log('Request has completed'),
+    complete: () => console.log('Request has completed' ),
   });
 
 }
